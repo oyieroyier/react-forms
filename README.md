19cd3dbd3b0597ebbafe65fe6d59116c6558a4ca
Bob Oyier <boboyier92@gmail.com>
2023-05-23 23:33:45 +0300

    Basic form validation using react-hook-form

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Using the React Hook Form and Zod libraries in React forms.
+
+Commit 1: Using the react-hook-form to capture form data.
+
+Commit 2: Using the react-hook-form for validation.
+
+Commit 3: Replacing the basic react-hook-form validation with schema-based Zod validation.
+
+Check the console logs for debugging.
--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -2,10 +2,21 @@ import { FieldValues, useForm } from 'react-hook-form';
 
 // Components
 import FormBackgroundAndShapes from './FormBackgroundAndShapes';
-import FormSocialMediaLinks from './FormSocialMediaLinks';
+/* import FormSocialMediaLinks from './FormSocialMediaLinks'; */
+
+interface FormData {
+	name: string;
+	pin: number;
+}
 
 const Form = () => {
-	const { register, handleSubmit } = useForm();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>();
+
+	console.log(errors);
 
 	const onSubmit = (data: FieldValues) => {
 		console.log(data);
@@ -19,23 +30,34 @@ const Form = () => {
 
 				<label htmlFor="username">Username</label>
 				<input
-					{...register('name')}
+					{...register('name', { required: true, minLength: 3 })}
 					type="text"
 					placeholder="Email or Phone"
 					id="username"
 				/>
+				{errors.name?.type === 'required' && <p>The name field is required</p>}
+				{errors.name?.type === 'minLength' && (
+					<p>The name field must be at least 3 characters long</p>
+				)}
 
 				<label htmlFor="password">PIN</label>
 				<input
-					{...register('pin')}
+					{...register('pin', { required: true, minLength: 6 })}
 					type="password"
 					placeholder="PIN"
-					id="password"
+					id="pin"
 				/>
+				{errors.pin?.type === 'required' && <p>The PIN field is required</p>}
+				{errors.pin?.type === 'minLength' && (
+					<p>The PIN field must be at least 6 characters long</p>
+				)}
 
 				<button>Log In</button>
 
-				<FormSocialMediaLinks />
+				{/* You can uncomment the component below if you want to view it after cloning the repo.
+				Remember to uncomment the import statement above too. */}
+
+				{/* <FormSocialMediaLinks /> */}
 			</form>
 		</>
 	);
